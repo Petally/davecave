6fb44ad5573a5726ab2e1e734b2abd3ca1756f8e
davidslevs <44510876+Petally@users.noreply.github.com>
2022-11-20 02:41:28 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 window.addEventListener("load", () => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
+    const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
     //resizing
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight * 0.75;
+    canvas.width = window.innerWidth * 0.75;
 
     //variables
     let painting = false;
+    let brushSize = 2;
+
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
 
     function startPosition(e) {
         painting = true;
@@ -18,19 +28,32 @@ window.addEventListener("load", () => {
         ctx.beginPath();
     }
 
-    function draw(e) {
-        if (!painting) return;
-        ctx.lineWidth = 10;
-        ctx.lineCap = 'round';
+    function draw(event) {
+        var e = getMousePos(canvas, event);
 
-        ctx.lineTo(e.clientX, e.clientY)
+        if (!painting) return;
+        ctx.lineWidth = brushSize;
+        ctx.lineCap = 'round';
+        ctx.lineTo(e.x, e.y)
         ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(e.clientX, e.clientY);
+        ctx.moveTo(e.x, e.y);
+    }
+
+    function changeSize(e) {
+        if (e.deltaY < 0) {
+            brushSize = clamp(brushSize - 2, 2, 20)
+        } else {
+            brushSize = clamp(brushSize + 2, 2, 20)
+        }
     }
     //EventListeners
     canvas.addEventListener('mousedown', startPosition);
     canvas.addEventListener('mouseup', finishedPosition);
     canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('wheel', function(e){
+        changeSize(e);
+        return false; 
+    }, false);
 });
 
